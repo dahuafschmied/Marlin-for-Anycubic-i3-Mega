50f30689691716d76a1ca36e30760611770bd786
dahuafschmied <>
2019-06-29 10:47:25 +0200

    Travel Acc = Printing Acc

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -727,7 +727,7 @@
  */
 #define DEFAULT_ACCELERATION          1500    // X, Y, Z and E acceleration for printing moves
 #define DEFAULT_RETRACT_ACCELERATION  1000    // E acceleration for retracts
-#define DEFAULT_TRAVEL_ACCELERATION   2000    // X, Y, Z acceleration for travel (non printing) moves
+#define DEFAULT_TRAVEL_ACCELERATION   1500    // X, Y, Z acceleration for travel (non printing) moves
 
 //
 // Use Junction Deviation instead of traditional Jerk Limiting
