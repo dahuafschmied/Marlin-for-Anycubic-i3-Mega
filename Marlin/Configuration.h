3f28e783ea49c8a36fd268fbd3e0d957c9b3cdac
dahuafschmied <>
2019-12-20 20:08:05 +0100

    Nozzel offset to -1.80

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -916,7 +916,7 @@
  *
  * Specify a Probe position as { X, Y, Z }
  */
-#define NOZZLE_TO_PROBE_OFFSET { -25, 20, -1.68 }
+#define NOZZLE_TO_PROBE_OFFSET { -25, 20, -1.80 }
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 26
